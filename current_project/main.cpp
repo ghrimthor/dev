188e61faa818b1b9ba3b0a104637129f4e4b0201
ghrimthor <yannick.martz@gmail.com>
2019-03-19 09:25:49 -0700

    class and class variables

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 class FirstClass{
+  private:  // declare variable private usually
+    string name; // init of name variable
+
   public:  // anything after that usable outside the class
-    void coolSaying() {
-        cout << "Hello Class" << endl;
+    FirstClass(){ // this is a constructor same name as the class but a function (never have a return type, no void to put)
+      cout << "This is a constructor, will get print automatically when an object from the class is used. It is usually used to init variables" << endl;
+    }
+    void setName(string str){ //set function to set our variable name
+      name = str;
+    }
+    string getName(){ //get function to get our variable name
+      return name;
     }
 };
 
-void printSomething(){
-  cout << "Hello World" << endl;
-}
 
 int main() {
   /* code */
   FirstClass firstObject;
-  firstObject.coolSaying();
-  printSomething();
+  firstObject.setName("Yannick Martz \n");
+  cout << firstObject.getName();
+
 
 }
