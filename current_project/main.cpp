b2eeca5af9f3a511e71bdac81017e35d11ce294e
ghrimthor <yannick.martz@gmail.com>
2019-03-19 09:57:19 -0700

    constructor usage example, init of a string

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -4,35 +4,26 @@ using namespace std;
 
 
 class FirstClass{
-
   private:  // declare variable private usually
     string name; // init of name variable
 
   public:  // anything after that usable outside the class
-
+    FirstClass(string str){ // this is a constructor same name as the class but a function (never have a return type, no void to put)
+      setName(str);
+        }
     void setName(string str){ //set function to set our variable name
       name = str;
     }
     string getName(){ //get function to get our variable name
       return name;
     }
-
-    void coolSaying() {
-        cout << "Hello Class" << endl;
-    }
 };
 
-void printSomething(){
-  cout << "Hello World" << endl;
-}
 
 int main() {
   /* code */
-  FirstClass firstObject;
-  firstObject.setName("Yannick Martz \n");
-  cout << firstObject.getName();
+  FirstClass firstObject("Yannick Martz Junior");
+    cout << firstObject.getName();
 
-  firstObject.coolSaying();
-  printSomething();
 
 }
