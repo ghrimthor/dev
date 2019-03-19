4996bab4776daf0907fa1fb0399e63452d3b9f02
ghrimthor <yannick.martz@gmail.com>
2019-03-19 09:25:49 -0700

    class and class variables

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 class FirstClass{
+
+  private:  // declare variable private usually
+    string name;
+
   public:  // anything after that usable outside the class
+
+    void setName(string str){ //setter function
+      name = str;
+    }
+    string getName(){ //
+      return name;
+    }
+
     void coolSaying() {
         cout << "Hello Class" << endl;
     }
@@ -16,6 +29,9 @@ void printSomething(){
 int main() {
   /* code */
   FirstClass firstObject;
+  firstObject.setName("Yannick Martz \n");
+  cout << firstObject.getName();
+
   firstObject.coolSaying();
   printSomething();
 
