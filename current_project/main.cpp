d75f232f36b3d993512b15ae8dee3489e191128e
ghrimthor <yannick.martz@gmail.com>
2019-03-19 07:59:01 -0700

    add class

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void printSomething();
 
-int main() {
-  /* code */
-  printSomething();
-
-}
+class FirstClass{
+  public:  // anything after that usable outside the class
+    void coolSaying() {
+        cout << "Hello Class" << endl;
+    }
+};
 
 void printSomething(){
   cout << "Hello World" << endl;
+}
+
+int main() {
+  /* code */
+  FirstClass firstObject;
+  firstObject.coolSaying();
+  printSomething();
+
 }
