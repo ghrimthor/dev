c718c5992427255caca207de4c51a825d9acdce4
ghrimthor <yannick.martz@gmail.com>
2019-03-20 10:02:03 -0700

    do_while loop demo

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main() {
 
-  float a;
-  float p = 10000;
-  float r = .01;
+  int x = 1;
 
-  for(int day = 1; day <= 20; day++){
-    a = p * pow(1+r, day);
-    cout << day << "----" << a << endl;
+  // advantage: will at least run one time
+
+  do{
+    cout << x << endl;
+    x++;
   }
-
+  while(x<10);
 
 
 }
