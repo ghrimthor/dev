a45c3e31be6d0eea74c6929aaa1b6dcd4c44b66a
ghrimthor <yannick.martz@gmail.com>
2019-03-26 07:51:22 -0700

    pointer basic

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -1,34 +1,13 @@
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
 
 
-// User defined sizeof macro
-# define my_sizeof(type) ((char *)(&type+1)-(char*)(&type))
-
-void printArray(int array[]){ //argument array name[]
-  int sizeOfArray=my_sizeof(array)/my_sizeof(array[0]);
-  for (int i = 0; i < sizeOfArray; i++){
-    cout << array[i] << endl;
-  }
-}
-
 int main(){
-  int testarray[6] = {2,6,6,8,10,85};
-  int MDarray [2][3] = {{2,3,4},{8,9,10}};
+  int fish = 5;
+  cout << &fish << endl; //access memory adress of fish
 
-  // 2,3,4
-  // 8,9,10
+  int *fishPointer;
+  fishPointer = &fish;
 
-  printArray(testarray);
-  cout << MDarray[0][2]; // to access 4  [1][1] to access 9
-
-  //To print MDarray
-  for(int row = 0; row < 2; row++){
-    for(int column = 0; column < 3; column++){
-      cout << MDarray[row][column] << " ";
-    }
-    cout << endl;
-  }
-
+  cout << fishPointer << endl;
 }
