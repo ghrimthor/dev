f69e07ed6a8c0edc3e4bc35a26ccd84efd9bbf93
ghrimthor <yannick.martz@gmail.com>
2019-03-26 07:32:05 -0700

    MDarray

--- current_project/main.cpp
+++ current_project/main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void printArray(int theArray[],int sizeOfArray){ //argument array name[]
+
+// User defined sizeof macro
+# define my_sizeof(type) ((char *)(&type+1)-(char*)(&type))
+
+void printArray(int array[]){ //argument array name[]
+  int sizeOfArray=my_sizeof(array)/my_sizeof(array[0]);
   for (int i = 0; i < sizeOfArray; i++){
-    cout << theArray[i] << endl;
+    cout << array[i] << endl;
   }
 }
 
 int main(){
-//------------------------------------------------------------------------------
-    // int array[9];
-    // int sum = 0;
-    //
-    // cout << "Element - Value" << endl;
-    //
-    // for(int i=0; i<9 ; i++){
-    //   array[i] = 99;
-    //   cout << i << " ------ " << array[i] << endl;
-    //   sum += array[i]; // sum = sum + array[i]
-    // }
-    // cout << sum << endl;
-//------------------------------------------------------------------------------
-//with function printArray
-  int otherArray[3]={2,4,5};
-  printArray(otherArray, (sizeof(otherArray)/sizeof(*otherArray)));
+  int testarray[6] = {2,6,6,8,10,85};
+  int MDarray [2][3] = {{2,3,4},{8,9,10}};
+
+  // 2,3,4
+  // 8,9,10
+
+  printArray(testarray);
+  cout << MDarray[0][2]; // to access 4  [1][1] to access 9
+
+  //To print MDarray
+  for(int row = 0; row < 2; row++){
+    for(int column = 0; column < 3; column++){
+      cout << MDarray[row][column] << " ";
+    }
+    cout << endl;
+  }
+
 }
